c75d805daddfb7e77226cfb9caf87c1815186460
Yabadaba <allenbao01@gmail.com>
2021-11-30 21:22:23 -0800

    first commit

---
+++ TodayIs.java
@@ -0,0 +1,18 @@
+/*
+ * Author: Allen Bao
+ * Date: 9/27
+ * File: TodayIs.java
+ *
+ * Simple Java program that tells you the month, day, and day of the month
+ */
+class TodayIs {
+
+    public static void main(String[] args) {
+        int day = 27;
+        int month = 9;
+        System.out.println("The month is September");
+        System.out.println("September is month number " + month);
+        System.out.println("Today is the " + day + "th day of the month");
+        
+    }
+}
